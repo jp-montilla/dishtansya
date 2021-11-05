4c6b5e9ea34d1206216c33630b41588041c5d0b6
John Paul Montilla <yoqweqw143@gmail.com>
2021-11-05 10:44:51 +0800

    Email order placed

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Http\Requests\OrderRequest;
-use App\Mail\MyTestMail;
-use App\Models\Order;
-use App\Models\Product;
+use App\Mail\OrderMail;
 use App\Repositories\OrderRepo;
 use App\Repositories\ProductRepo;
 use Illuminate\Http\Request;
@@ -26,7 +24,7 @@ class OrderController extends Controller
         foreach ($fields['products'] as $product)
         {
             $product_data = $this->productRepo->findById($product['product_id']);
-            
+
             if ($product_data->available_stock < $product['quantity'])
             {
                 return response([
@@ -38,6 +36,8 @@ class OrderController extends Controller
         $order = $this->orderRepo->create();
 
         $order->products()->attach($this->orderRepo->mapProducts($fields['products']));
+
+        \Mail::to($order->user->email)->send(new OrderMail($order));
 
         return response([
             'message' => 'You have successfully ordered this products.',
